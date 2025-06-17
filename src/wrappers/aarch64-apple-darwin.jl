2c8b371983b04f26a361374467ec379ffdca60cf
jlbuild <juliabuildbot@gmail.com>
2025-06-17 15:41:41 +0000

    GAP_pkg_deepthought_jll build 100.0.701+0

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -2,11 +2,10 @@
 export deepthought
 
 using GAP_jll
-using GAP_lib_jll
 JLLWrappers.@generate_wrapper_header("GAP_pkg_deepthought")
 JLLWrappers.@declare_file_product(deepthought)
 function __init__()
-    JLLWrappers.@generate_init_header(GAP_jll, GAP_lib_jll)
+    JLLWrappers.@generate_init_header(GAP_jll)
     JLLWrappers.@init_file_product(
         deepthought,
         "lib/gap/DeepThought.so",
